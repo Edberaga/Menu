d8a36e36690258a56987930f6fb80623e8ce300f
edberaga <edbetjonnathan@gmail.com>
2024-09-04 19:23:58 +0700

    Foods

---
+++ src/components/foods/desserts.jsx
@@ -0,0 +1,16 @@
+import MenuItem from "../MenuItem";
+
+export const desserts = (
+    <>
+      <MenuItem
+        name={"Ice Cream"}
+        price={"RM 5.00"}
+        nutritionInfo={{ calories: 207, protein: 2, carbs: 33, fat: 6 }}
+      />
+      <MenuItem
+        name={"Apple Pie"}
+        price={"RM 2.00"}
+        nutritionInfo={{ calories: 270, protein: 1, carbs: 43, fat: 10 }}
+      />
+    </>
+  );
---
+++ src/components/foods/friedChicken.jsx
@@ -0,0 +1,16 @@
+import MenuItem from "../MenuItem";
+
+export const friedChicken = (
+    <>
+      <MenuItem
+        name={"2pc Fried Chicken Set"}
+        price={"RM 10.00"}
+        nutritionInfo={{ calories: 540, protein: 31, carbs: 35, fat: 29 }}
+      />
+      <MenuItem
+        name={"6 piece nugget"}
+        price={"RM 6.00"}
+        nutritionInfo={{ calories: 270, protein: 13, carbs: 16, fat: 16 }}
+      />
+    </>
+  );
