f85fe61a952d5887b196f33680981ffd5da1476b
edberaga <edbetjonnathan@gmail.com>
2024-09-04 19:24:34 +0700

    Develup menu app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Category from './components/Category';
+import { friedChicken } from './components/foods/friedChicken';
+import { desserts } from './components/foods/desserts';
 
 export default function App() {
   return (
-    <main>
-      <p>Hello world</p>
-    </main>
+  <main>
+    <h1>Uncle Haris Fried Chicken</h1>
+    <Category title={"Fried Chicken"} foods={friedChicken} />
+    <Category title={"Desserts"} foods={desserts} />
+  </main>
   );
 }
